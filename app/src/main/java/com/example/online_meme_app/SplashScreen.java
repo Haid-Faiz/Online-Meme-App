eabb2d12b52aa67b8f6a3887c7e622924a6c5089
Faizan Haider <haiderfaizan121@gmail.com>
2020-10-22 19:31:27 +0530

    Completed Meme App

---
+++ app/src/main/java/com/example/online_meme_app/SplashScreen.java
@@ -0,0 +1,17 @@
+package com.example.online_meme_app;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+    }
+}
